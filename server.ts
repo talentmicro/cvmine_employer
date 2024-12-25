539830b20ab49536911af857a728cd5ead2d3ac6
sampat-talentmicro <sampat@talentmicro.com>
2024-12-25 17:04:51 +0530

    pull req

--- server.ts
+++ server.ts
@@ -45,7 +45,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-    const port = process.env['PORT'] || 16020;
+    const port = process.env['PORT'] || 15020;
 
     // Start up the Node server
     const server = app();
